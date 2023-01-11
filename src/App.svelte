f75acdb1e363bb5d835960aced7cc759e18b738e
Germán Oyarzo <71647622+germanoyarzo@users.noreply.github.com>
2023-01-11 18:56:04 -0300

    login-page

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { Router, Link, Route, navigate } from "svelte-routing"
+	import Login from "./pages/Login.svelte"
+	import Home from "./pages/Home.svelte"
+	let user = null
+	var previousPath
+	$: user
+
+
+	function loggedIn (event) {
+		if (event) {
+			user = { email: "germa@gmail.com",
+					 password: "1234"	
+					}
+			navigate("/home")
+			console.log(user)
+		}
+	}
+	function loggingIn() {
+		previousPath = window.location.pathname
+	}
+
+	
+	</script>
+	 
+	 <style>
+		section {
+		  height: 100vh;
+		  width: 100%;
+		  display: flex;
+		  justify-content: center;
+		  align-items: center;
+		  background: linear-gradient(to right, #a9d39e, #9ed9e4);
+		}
+	  </style>
+  <section>
+
+  
+	<Router>
+		<main>
+			<Route path="/"><Login on:loggedIn={loggedIn} /></Route>
+			{#if user !== null}
+			<Route path="/home"><Home /></Route>
+			{:else}
+			<Route path="/"><Login on:loggedIn={loggedIn} /></Route>
+			{/if}
+		</main>
+	</Router>
+</section>	
---
+++ src/pages/Home.svelte
@@ -0,0 +1,51 @@
+<script>
+    import { Router, Link, Route, navigate } from "svelte-routing"
+	import Login from "./Login.svelte"
+	import Home from "./Home.svelte"
+
+    let user = null
+    var previousPath
+    $: user
+   
+    function logout() {
+      localStorage.clear()
+      user = null
+      navigate("/")
+	  }
+</script>
+
+<style>
+    form {
+      background: linear-gradient(to right, #a9d39e, #9ed9e4);
+      padding: 4px;
+      width: 100%;
+      height: 90px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      box-shadow: 0px 20px 14px 8px rgba(0, 0, 0, 0.58);
+    }
+    input {
+      border: none;
+      border-bottom: 1px solid #ccc;
+      margin-bottom: 20px;
+      transition: all 300ms ease-in-out;
+      width: 100%;
+    }
+    input:focus {
+      outline: 0;
+      border-bottom: 1px solid #666;
+    }
+   
+  </style>
+<main>
+    <h2 align="center">Welcome</h2>
+    <nav>
+        <form on:submit={logout}>
+            <input type="submit" value="Logout" />
+        </form>
+        
+    </nav>
+    
+</main>
