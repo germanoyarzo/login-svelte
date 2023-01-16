73633528674292fd10980e0213637af59438ced2
Germán Oyarzo <71647622+germanoyarzo@users.noreply.github.com>
2023-01-16 14:29:11 -0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,66 +5,28 @@
 	import users from './Data/File.json';
   	import Register from "./pages/Register.svelte";
 
-	console.log(users)
-	
-
-	let user = null
-	var previousPath
-	$: user
-
-
-	
-	function loggedIn (event) {
-		if (event) {
-			user = { email: "",
-					 password: ""	
-					}
-			navigate("/home")
-			console.log(user)
-		}
-	}
-
-	function registerIn (event) {
-		if (event) {
-			user = { email: "",
-					 password: "",
-					 name:"",
-					 cellphone:""
-					}
-			navigate("/home")
-			console.log(user)
-		}
-	}
-	function loggingIn() {
-		previousPath = window.location.pathname
-	}
-
-	
-	</script>
+</script>
 	 
-	 <style>
-		section {
-		  height: 100vh;
-		  width: 100%;
-		  display: flex;
-		  justify-content: center;
-		  align-items: center;
-		  background: linear-gradient(to right, #a9d39e, #9ed9e4);
-		}
-	  </style>
-  <section>
+ <style>
+	section {
+	  height: 100vh;
+	  width: 100%;
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  background: linear-gradient(to right, #a9d39e, #9ed9e4);
+	}
+  </style>
+<section>
 
-  
-	<Router>
-		<main>
-			<Route path="/"><Login on:loggedIn={loggedIn} /></Route>
-			{#if user !== null}
-			<Route path="/home"><Home /></Route>
-			
-			{:else}
-			<Route path="/"><Login on:loggedIn={loggedIn} /></Route>
-			{/if}
-			<Route path="/register"><Register on:registerIn={registerIn}/></Route>
-		</main>
-	</Router>
+
+<Router>
+	<main>
+
+	  <Route path="/home" component={Home} />
+	  <Route path="/" component={Login} />
+	  <Route path="/register" component={Register} />
+
+	</main>
+</Router>
 </section>	
